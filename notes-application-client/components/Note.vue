db256e9fa12a5da9696e99b108672816525787e9
Britney Bogard <britney.devs@gmail.com>
2020-07-31 22:29:20 +0200

    View, create and delete working

--- notes-application-client/components/Note.vue
+++ notes-application-client/components/Note.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="component">
+  <div class="tile is-parent is-vertical is-4">
+    <div class="tile is-child notification is-primary">
+      <p class="content">{{ this.note.noteText }}</p>
+      <a @click="deleteNote">delete</a>
+    </div>
   </div>
 </template>
 
 <script>
+import { API } from '@aws-amplify/api';
+
 export default {
+  name: 'Note',
   props: ['note'],
+  methods: {
+    async deleteNote() {
+      const path = `/note/${this.note.id}`;
+      await API.del('NotesBackend', path);
+      this.$emit('delete-note');
+    },
+  },
 };
 </script>
--- notes-application-client/pages/notes/index.vue
+++ notes-application-client/pages/notes/index.vue
@@ -1,55 +1,54 @@
 <template>
-  <div class="container">
-    <div class="hero-body">
-      <div class="column is-6 is-offset-3">
-        <amplify-authenticator
-          v-if="authState !== 'signedin'"
-        />
-        <div
-          v-if="authState === 'signedin' && user"
-          class="box has-text-centered">
-          YOU're AUTHd
-        </div>
+  <section class="section">
+    <div class="container">
+      <amplify-authenticator
+        v-if="authState !== 'signedin'"
+      />
+      <div class="notes tile is-ancestor is-12" v-if="authState === 'signedin' && user">
+        <Note v-for="note of notes" :note="note" :key="note.id" v-on:delete-note="loadNotes"  />
+        <CreateNote v-on:create-note="loadNotes" />
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import { onAuthUIStateChange } from '@aws-amplify/ui-components';
 import { API } from '@aws-amplify/api';
+import Note from '../../components/Note.vue';
+import CreateNote from '../../components/CreateNote.vue';
 
 export default {
+  components: {
+    Note,
+    CreateNote,
+  },
   async created() {
     onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData;
     });
 
-    // const path = '/note';
-    // const apiName = 'NotesBackend';
-    // const request = {
-    //   body: {
-    //     text: 'test',
-    //   },
-    // };
-
-    // const response = await API.post(apiName, path, request);
-
-    const path = '/notes';
-    const apiName = 'NotesBackend';
-
-    const response = await API.get(apiName, path);
-    console.log(response);
+    return this.loadNotes();
   },
   data() {
     return {
       user: undefined,
       authState: undefined,
+      notes: [],
     };
   },
   beforeDestroy() {
     return onAuthUIStateChange;
+  },
+  methods: {
+    async loadNotes() {
+      const path = '/notes';
+      const apiName = 'NotesBackend';
+
+      const response = await API.get(apiName, path);
+      this.notes = response;
+    },
   },
 };
 </script>
@@ -57,5 +56,9 @@ export default {
 <style>
 html {
   background-color: #dddddd;
+}
+
+.notes {
+  flex-wrap: wrap;
 }
 </style>
